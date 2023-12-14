6e0e7a3693ad07b8345d38cfcc39cbb4fd377834
kenmaro3 <miha.ken.19@gmail.com>
2023-12-14 09:23:31 +0900

    first commit

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+open class MyClass{
+    public var name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+}
